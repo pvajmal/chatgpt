1b3fa81926cf8d81ec05c798c446c3a565f893eb
pvajmal29 <pvajmal29@gmail.com>
2023-01-10 18:23:02 +0530

    'jan10'

--- sam.py
+++ sam.py
@@ -45,7 +45,7 @@ if proceed:
    expensefinal = pd.concat(expense1, expense2)
    expensefinal.to_excel('Expenses.csv')
 expense1 = pd.read_csv('/app/chatgpt/Expenses.csv')
-st.markdown(f"Your name is **{expense1.shape()}**")
+st.markdown(f"Your name is **{expense1.shape[1]}**")
 # Set the file to be downloaded
 file = '/app/chatgpt/Expenses.csv'
 
