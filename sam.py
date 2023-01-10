6c7d8a067347897c036ba53786ff04a8c6331bad
pvajmal29 <pvajmal29@gmail.com>
2023-01-10 17:33:42 +0530

    'jan10'

--- sam.py
+++ sam.py
@@ -3,31 +3,34 @@ import pandas as pd
 import os
 
 # Set the page title
-st.title("Resume Builder")
+st.title("Expense calculator")
 
 # Add a header
-st.header("Welcome to the resume builder!")
+st.header("Welcome to Expense calculator!")
 
 # Add a subheader
-st.subheader("Enter your information below to create your resume.")
+st.subheader("Enter your information below to create your Expense calculator")
 
 # Add a text input field for the user's name
 name = st.text_input("Name", "Enter your name here")
+options = ["Food", "Entertainment", "Dress"]
+selected_option = st.selectbox("Select an option:", options,index=1)
 
+st.write("You selected:", selected_option)
 # Add a text input field for the user's email
-email = st.text_input("Email", "Enter your email here")
+amount = st.text_input("Email", "Enter amount here")
 
 # Add a text input field for the user's job title
-job_title = st.text_input("Job Title", "Enter your job title here")
+#job_title = st.text_input("Job Title", "Enter your job title here")
 
 # Add a text area for the user's job description
-job_description = st.text_area("Job Description", "Enter your job description here")
+#job_description = st.text_area("Job Description", "Enter your job description here")
 
 # Add a button to submit the form
-st.button("Create Resume")
+st.button("Add expense")
 
 # Display a success message
-st.success("Thank you for creating your resume!")
+st.success("Thank you for your submission!")
 
 #output
 st.markdown(f"Your name is **{os.getcwd(), os.listdir()}**")
@@ -36,9 +39,10 @@ st.markdown(f"Your name is **{os.getcwd(), os.listdir()}**")
 download_button = st.button("Download file")
 
 df = pd.DataFrame()
-df['Ajmal'] = 'name'
+df[name] = name
 
-#df.to_excel('aju.xlsx')
+
+df.to_csv('aju.csv')
 # Set the file to be downloaded
 file = '/app/chatgpt/aju.csv'
 
